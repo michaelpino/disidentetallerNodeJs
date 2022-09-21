24a3c2ac913eccbb2b45d1893265d648fe4c5d75
Michael Pino <the.nemesis.7@gmail.com>
2022-09-21 00:20:06 -0300

    Desafio 5 - Templates: Agregados templates de vistas con Handlebars

--- Contenedor.js
+++ Contenedor.js
@@ -1,72 +1,58 @@
-const fs = require('fs');
-
-export default class Contenedor {
-    constructor(nombreArchivo) {
-        this.nombreArchivo = nombreArchivo;
+class Contenedor {
+    constructor() {
+        this.productos = [];
     }
     
-    async saveOnFile(listadoProductos){
-        try {
-            await fs.promises.writeFile(`./${this.nombreArchivo}`, JSON.stringify(listadoProductos, null, 2), 'utf-8');
-          } catch (err) {
-            console.log("Error al intentar escribir en el archivo");
-            console.log(err);
-          }
-    }
-    
-    async save(nuevoProducto){
-        const listado = await this.getAll();
-        let ultimoId = 0;
-        if (listado.length > 0){
-            listado.forEach(elemento => {
-                if(elemento.id > ultimoId){
-                    ultimoId = elemento.id;
-                }
-            });
+    save(nuevoProducto){
+        if(this.productos.length === 0){
+            nuevoProducto.id = 1;
         }
-        nuevoProducto.id = ultimoId + 1;
-        listado.push(nuevoProducto);
-        await this.saveOnFile(listado);
+        else {
+            nuevoProducto.id = this.productos[this.productos.length - 1].id + 1;
+        }
+        this.productos.push(nuevoProducto);
         return nuevoProducto.id;
     }
 
-    async getAll(){
-        try{
-            const data = await fs.promises.readFile(`./${this.nombreArchivo}`, 'utf-8');
-            if(data === "" || data === "[]"){
-                console.log("Leí algo vacio");
-                return [];
+    getAll(){
+        return this.productos;
+    }
+
+    update(id, datosAActualizar){
+        let productoNoEncontrado = true;
+        let nuevoListado = this.productos.map((producto) => {
+            if (producto.id === id){
+                producto.title = datosAActualizar.title;
+                producto.price = datosAActualizar.price;
+                producto.thumbnail = datosAActualizar.thumbnail;
+                productoNoEncontrado = false;
             }
-            else {
-                return JSON.parse(data);
-            }
-        }catch (err){
-            //En caso que no se encontró el archivo
-            console.log(err);
-            return [];
+            return producto;
+        });
+        this.productos = [...nuevoListado];
+        if (productoNoEncontrado){
+            return { error : 'ERROR 404: producto no encontrado' };
         }
     }
 
-    async getById(idBuscado){
-        const listado = await this.getAll();
-        const productoBuscado = listado.filter((producto) => producto.id === idBuscado);
+    getById(idBuscado){
+        const productoBuscado = this.productos.filter((producto) => producto.id === idBuscado);
         if(productoBuscado.length > 0){
-            console.log(productoBuscado[0]);
             return productoBuscado[0];
         }
         else {
-            console.log("Producto no encontrado");
-            return null;
+            return { error : 'ERROR 404: producto no encontrado' };
         }
     }
 
-    async deleteById(idAEliminar){
-        const listado = await this.getAll();
-        const listadoFiltrado = listado.filter((producto) => producto.id !== idAEliminar);
-        await this.saveOnFile(listadoFiltrado);
+    deleteById(idAEliminar){
+        const listadoFiltrado = this.productos.filter((producto) => producto.id !== idAEliminar);
+        this.productos = [...listadoFiltrado];
     }
     
-    async deleteAll(){
-        await this.saveOnFile("[]");
+    deleteAll(){
+        this.productos = [];
     }
 }
+
+module.exports = Contenedor;
--- index.js
+++ index.js
@@ -1,63 +1,9 @@
 const express = require('express');
-//import Contenedor from "./Contenedor.js";
+const handlebars = require("express-handlebars");
+const Contenedor = require("./Contenedor.js");
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
-
-class Contenedor {
-    constructor() {
-        this.productos = [];
-    }
-    
-    save(nuevoProducto){
-        nuevoProducto.id = this.productos.length + 1;
-        this.productos.push(nuevoProducto);
-        return nuevoProducto.id;
-    }
-
-    getAll(){
-        return this.productos;
-    }
-
-    update(id, datosAActualizar){
-        let productoNoEncontrado = true;
-        let nuevoListado = this.productos.map((producto) => {
-            if (producto.id === id){
-                producto.title = datosAActualizar.title;
-                producto.price = datosAActualizar.price;
-                producto.thumbnail = datosAActualizar.thumbnail;
-                productoNoEncontrado = false;
-            }
-            return producto;
-        });
-        this.productos = [...nuevoListado];
-        if (productoNoEncontrado){
-            return { error : 'producto no encontrado' };
-        }
-    }
-
-    getById(idBuscado){
-        const productoBuscado = this.productos.filter((producto) => producto.id === idBuscado);
-        if(productoBuscado.length > 0){
-            console.log(productoBuscado[0]);
-            return productoBuscado[0];
-        }
-        else {
-            console.log("Producto no encontrado");
-            return { error : 'producto no encontrado' };
-        }
-    }
-
-    deleteById(idAEliminar){
-        const listadoFiltrado = this.productos.filter((producto) => producto.id !== idAEliminar);
-        this.productos = [...listadoFiltrado];
-    }
-    
-    deleteAll(){
-        this.productos = [];
-    }
-}
 
 
 const PORT = 8080;
@@ -67,43 +13,68 @@ const server = app.listen(PORT, () => {
 
 let inventario = new Contenedor("productos.txt");
 
+//CONFIGURACIÓN DE HANDLEBARS
+app.engine(
+    "hbs",
+    handlebars({
+        extname: ".hbs",
+        defaultLayout: "index.hbs",
+    })
+);
+app.set("view engine", "hbs");
+app.set("views", "./views");
+ 
+
 app.use(express.static("public"));
 
 const routerProductos = express.Router();
 
-
-
+routerProductos.get('/vista', (req, res) => {
+    const productos = inventario.getAll();
+    let hayProductos = false;
+    if (productos.length > 0){
+        hayProductos = true;
+    }
+    
+    res.render("vista", {
+        productos: productos,
+        mostrarProductos: hayProductos,
+    });
+});
 
 routerProductos.get('/', (req, res) => {
     const productos = inventario.getAll();
-    res.send(productos)
+    res.json(productos)
 });
 
 routerProductos.get('/:id', (req, res) => {
     const productoBuscado = inventario.getById(parseInt(req.params.id));
-    res.send(productoBuscado);
+    res.json(productoBuscado);
 });
 
 routerProductos.post('/', (req, res) => {
     const idAsignado = inventario.save(req.body);
-    req.body.id = idAsignado;
-    res.send(req.body);
+    const {title, price, thumbnail} = req.body;
+    res.json({id: idAsignado, title: title, price: price, thumbnail:thumbnail});
 });
 
 routerProductos.delete('/:id', (req, res) => {
     inventario.deleteById(parseInt(req.params.id));
-    res.send(`Producto con ID ${parseInt(req.params.id)} fue eliminado`);
+    res.json(`Producto con ID ${parseInt(req.params.id)} fue eliminado`);
 });
 
 routerProductos.put('/:id', (req, res) => {
     inventario.update(parseInt(req.params.id),req.body);
-    res.send(inventario.getById(parseInt(req.params.id)));
+    res.json(inventario.getById(parseInt(req.params.id)));
 });
 
 routerProductos.get('/productoRandom', (req, res) => {
     const productos = inventario.getAll();
     let productoRandom = productos[Math.floor(Math.random()*productos.length)];
-    res.send(productoRandom)
+    res.json(productoRandom)
 });
+
+
+
 
 app.use("/api/productos/", routerProductos);   
