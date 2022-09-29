a4fd2651d96138238f35e15cfe033bef0795069c
Michael Pino <the.nemesis.7@gmail.com>
2022-09-28 22:59:55 -0300

    Desafio Clase 12 - Desafio completado, sin Handlebars ni fetch

--- desafioC12/public/js/main.js
+++ desafioC12/public/js/main.js
@@ -1,23 +1,60 @@
 const socket = io.connect();
 
-socket.io("carga_inventario", (data)=>{
-    console.log(data);
-    render(data);
+socket.on("carga_inventario", (data)=>{
+    render_productos(data);
 })
 
-function render(data){
+socket.on("carga_chat", (data)=>{
+    render_chat(data);
+})
+
+function render_productos(data){
     const html = data.map((producto)=>{
         return `
-            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                ${producto.title}
-            </th>
-            <td class="py-4 px-6">
-                ${producto.price}
-            </td>
-            <td class="py-4 px-6">
-                <img width="30" src=${producto.thumbnail}>
-            </td>
+            <tr class="bg-white dark:bg-gray-800">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    ${producto.title}
+                </th>
+                <td class="py-4 px-6">
+                    ${producto.price}
+                </td>
+                <td class="py-4 px-6">
+                    <img width="30" src=${producto.thumbnail}>
+                </td>
+            </tr>
         `;
-    });
+    })
+    .join(" ");
     document.getElementById("listado_productos").innerHTML = html;
+}
+
+function render_chat(data){
+    const html = data.map((mensaje)=>{
+        return `
+            <div class="inline-flex">
+                <p class="px-3 text-blue-600 font-bold"> ${mensaje.mail} </p>
+                <p class=" text-yellow-800"> [ ${mensaje.date} ] : </p>
+                <p class="px-3 text-green-600 italic"> ${mensaje.message} </p>
+            </div>
+            
+        `;
+    })
+    .join(" <br> ");
+    document.getElementById("chat").innerHTML = html;
+}
+
+function agregaProducto(datos){
+    const nuevoProducto = {title: document.getElementById("title_input").value,
+                            price: document.getElementById("price_input").value,
+                            thumbnail: document.getElementById("thumbnail_input").value};
+    socket.emit("nuevo_producto", nuevoProducto);
+    return false;
+}
+
+function agregaMensaje(datos){
+    const nuevoMensaje = {mail: document.getElementById("chat-email_input").value,
+                            date: new Date().toLocaleString("es-CL"),
+                            message: document.getElementById("chat-mensaje_input").value};
+    socket.emit("nuevo_mensaje", nuevoMensaje);
+    return false;
 }
--- desafioC12/server.js
+++ desafioC12/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const handlebars = require("express-handlebars");
 const Contenedor = require("./Contenedor.js");
+const Tablero = require("./Tablero.js");
 const { Server: IOServer } = require("socket.io");
 const { Server: HttpServer } = require("http");
 
@@ -15,87 +15,33 @@ const PORT = 8080;
 
 app.use(express.static("public"));
 
-
-
-
-let inventario = new Contenedor("productos.txt");
+let inventario = new Contenedor();
 //AGREGO UNOS PRODUCTOS PARA TENERLOS PREVIAMENTE
 inventario.save({title: "Tijeras", price: 15, thumbnail:"https://cdn-icons-png.flaticon.com/512/4697/4697553.png"});
 inventario.save({title: "Post-it", price: 2, thumbnail:"https://cdn-icons-png.flaticon.com/512/4697/4697566.png"});
 inventario.save({title: "Crayones", price: 12, thumbnail:"https://cdn-icons-png.flaticon.com/512/4697/4697376.png"});
 
+//INICIALIZO EL TABLERO DE MENSAJES CON LOS MENSAJES GUARDADOS
+let tablero = new Tablero("mensajes.txt");
+tablero.readFile();
+
 io.on("connection", (socket) => {
     console.log("Se conectó un usuario!");
 
     socket.emit("carga_inventario", inventario.productos);
+
+    socket.emit("carga_chat", tablero.mensajes);
+
+    socket.on("nuevo_producto", (data) => {
+        inventario.save(data);
+        io.sockets.emit("carga_inventario", inventario.productos);
+    });
+
+    socket.on("nuevo_mensaje", (data) => {
+        tablero.save(data);
+        io.sockets.emit("carga_chat", tablero.mensajes);
+    });
 })
 
-const server = app.listen(PORT, () => {
-    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
-   });
 
-//CONFIGURACIÓN DE HANDLEBARS
-app.engine(
-    "hbs",
-    handlebars({
-        extname: ".hbs",
-        defaultLayout: "index.hbs",
-    })
-);
-app.set("view engine", "hbs");
-app.set("views", "./views");
- 
-
-
-
-const routerProductos = express.Router();
-
-routerProductos.get('/vista', (req, res) => {
-    const productos = inventario.getAll();
-    let hayProductos = false;
-    if (productos.length > 0){
-        hayProductos = true;
-    }
-    
-    res.render("vista", {
-        productos: productos,
-        mostrarProductos: hayProductos,
-    });
-});
-
-routerProductos.get('/', (req, res) => {
-    const productos = inventario.getAll();
-    res.json(productos)
-});
-
-routerProductos.get('/:id', (req, res) => {
-    const productoBuscado = inventario.getById(parseInt(req.params.id));
-    res.json(productoBuscado);
-});
-
-routerProductos.post('/', (req, res) => {
-    const idAsignado = inventario.save(req.body);
-    const {title, price, thumbnail} = req.body;
-    res.json({id: idAsignado, title: title, price: price, thumbnail:thumbnail});
-});
-
-routerProductos.delete('/:id', (req, res) => {
-    inventario.deleteById(parseInt(req.params.id));
-    res.json(`Producto con ID ${parseInt(req.params.id)} fue eliminado`);
-});
-
-routerProductos.put('/:id', (req, res) => {
-    inventario.update(parseInt(req.params.id),req.body);
-    res.json(inventario.getById(parseInt(req.params.id)));
-});
-
-routerProductos.get('/productoRandom', (req, res) => {
-    const productos = inventario.getAll();
-    let productoRandom = productos[Math.floor(Math.random()*productos.length)];
-    res.json(productoRandom)
-});
-
-
-
-
-app.use("/api/productos/", routerProductos);   
+httpServer.listen(PORT, () => console.log(`Servidor levantado en puerto ${PORT}`));
